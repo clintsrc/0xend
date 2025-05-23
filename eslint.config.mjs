aaa718f2dc8b67f9a1851b0ae95f97e144d6727f
Clint Jones <clinton.alan.jones@gmail.com>
2025-05-23 16:52:06 -0700

    ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    // ignore the compiled files, packages and this lint file itself
+    ignores: ["dist/**", "node_modules/**", "*.mjs"],
+  },
+  {
+    rules: {
+      quotes: ["error", "single"],
+    },
+  },
+  // Base JS setup with globals
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    ...js.configs.recommended,
+  },
+
+  // TypeScript rules
+  ...tseslint.configs.recommended,
+
+  // React JSX/TSX-specific rules
+  {
+    files: ["**/*.{jsx,tsx}"],
+    ...pluginReact.configs.flat.recommended,
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "prefer-const": "error",
+    },
+  },
+]);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,20 @@ import express from 'express';
 const app = express();
 const PORT = 3001;
 
+// "eslint.enable": false,  // to debug eslint configs
+const x = 1; // the auto-fix should change 'let' to 'const'
+
 // app.use(express.json());
 /* temporary: to verify the frontend is communicating with the backend */
 app.use((req, res) => {
   console.log(`${req.method} request to ${req.url}`);
-  res.json({ message: "Hello from the TypeScript server!" });
+  res.json({ message: 'Hello from the TypeScript server!' });
 });
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from the TypeScript server!' });
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
