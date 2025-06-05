072e6a62ac6a0d2c191acda08f66a82f76a6c4ef
Clint Jones <clinton.alan.jones@gmail.com>
2025-06-05 14:53:53 -0700

    Prometheus reports to Grafana

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,19 +2,35 @@ import dotenv from 'dotenv';
 dotenv.config(); // check the env for PORT
 
 import express from 'express';
+import client from 'prom-client';
 
 const app = express();
 const PORT = 3001;
 
+// Collect default (mem, cpu) metrics
+client.collectDefaultMetrics();
+
+// Basic health route
+app.get('/api/health', (_req, res) => {
+  res.json({ status: 'ok' });
+});
+
+// Expose /metrics for Prometheus
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
 // app.use(express.json());
-/* temporary: to verify the frontend is communicating with the backend */
+/* Default route:
+   temporary: to verify the frontend is communicating with the backend */
 app.use((req, res) => {
   console.log(`${req.method} request to ${req.url}`);
   res.json({ message: 'Hello from the TypeScript server!' });
-});
-
-app.get('/api/health', (_req, res) => {
-  res.json({ status: 'ok' });
 });
 
 if (process.env.NODE_ENV === 'production') {
@@ -22,6 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serves client's dist build output folder
   app.use(express.static('../client/dist'));
 }
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT.toString()}}`);
 });
